e29c53497961454915f9e1657787a1776e42bebb
Michal Paduch <mipa@noitso.dk>
2024-08-29 13:44:46 +0200

    Add clientId replacement

---
+++ apps/mptimer-app/src/environments/environment.prod.ts
@@ -0,0 +1,32 @@
+export const environment = {
+  msalConfig: {
+    auth: {
+      clientId: '#{APP_CLIENT_ID}#',
+    },
+  },
+  apiConfig: {
+    scopes: [''],
+    uri: '',
+  },
+  b2cPolicies: {
+    names: {
+      signUpSignIn: 'B2C_1_mptimersignupsignin',
+      resetPassword: 'B2C_1_PasswordReset',
+    },
+    authorities: {
+      signUpSignIn: {
+        authority:
+          'https://mptimer.b2clogin.com/mptimer.onmicrosoft.com/B2C_1_mptimersignupsignin',
+      },
+      signUp: {
+        authority:
+          'https://mptimer.b2clogin.com/mptimer.onmicrosoft.com/B2C_1_mptimersignup',
+      },
+      resetPassword: {
+        authority:
+          'https://mptimer.b2clogin.com/mptimer.onmicrosoft.com/B2C_1_PasswordReset',
+      },
+    },
+    authorityDomain: 'https://mptimer.b2clogin.com/mptimer.onmicrosoft.com/',
+  },
+};
